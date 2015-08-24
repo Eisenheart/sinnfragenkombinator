208d82baf012cfe7cdbd2fd232482ff542525c6e
name": null <email": null>
2015-08-23 19:03:33 -0700

    added three labels for questions view

--- app/sfk_questions_view_controller.rb
+++ app/sfk_questions_view_controller.rb
@@ -7,8 +7,15 @@ class SfkQuestionsViewController < UIViewController
   def viewDidLoad
     view.image = UIImage.imageNamed('epikouros.jpg') #needs to be compressed
     # view.image = CGRect(10,10,30,30); #images go in resources file
-    @label = makeQuestionLabel
-    view.addSubview(@label)
+
+    @labelIst = makeLabelIst(10,30,40,40)
+    @labelSubject = makeLabel(50,30,150,40)
+    @labelSecondPart = makeLabel(120,30,200,40)
+    @labelQuestionMark = makeLabelQuestionMark(250,30,20,40)
+    view.addSubview(@labelIst)
+    view.addSubview(@labelSubject)
+    view.addSubview(@labelSecondPart)
+    view.addSubview(@labelQuestionMark)
 
     view.userInteractionEnabled = true
     recognizer = UITapGestureRecognizer.alloc.initWithTarget(self, action:'showAQuestion')
@@ -23,26 +30,48 @@ class SfkQuestionsViewController < UIViewController
 
     UIView.animateWithDuration(1.0,
       animations: lambda {
-        @label.alpha = 0
-        @label.transform = CGAffineTransformMakeScale(0.1,0.1)
+        @labelSubject.alpha = 0
+        @labelSubject.transform = CGAffineTransformMakeScale(0.1,0.1)
       },
       completion:lambda { |finished|
-        @label.text = @sfkQuestion.randomQuestion
+        @labelSubject.text = @sfkQuestion.randomQuestion
         UIView.animateWithDuration(1.0,
                         animations:lambda {
-                          @label.alpha = 1
-                          @label.transform = CGAffineTransformIdentity
+                          @labelSubject.alpha = 1
+                          @labelSubject.transform = CGAffineTransformIdentity
                           })
         })
   end
 
-  def makeQuestionLabel
-    label = UILabel.alloc.initWithFrame([[10,60],[300,80]])
+  def makeLabel(x,y,width,height)
+    label = UILabel.alloc.initWithFrame([[x,y],[width,height]])
     label.backgroundColor = UIColor.lightGrayColor
     label.text = ['Europa','Aufräumen','Berlin','Leberwurst'].sample
-    label.font = UIFont.boldSystemFontOfSize(34)
+    label.font = UIFont.boldSystemFontOfSize(20)
     label.textColor = UIColor.darkGrayColor
     label.textAlignment = UITextAlignmentCenter
     label
   end
+
+  def makeLabelIst(x,y,width,height)
+    label = UILabel.alloc.initWithFrame([[x,y],[width,height]])
+    label.backgroundColor = UIColor.lightGrayColor
+    label.text = "Ist"
+    label.font = UIFont.boldSystemFontOfSize(20)
+    label.textColor = UIColor.darkGrayColor
+    label.textAlignment = UITextAlignmentCenter
+    label
+  end
+
+  def makeLabelQuestionMark(x,y,width,height)
+    label = UILabel.alloc.initWithFrame([[x,y],[width,height]])
+    label.backgroundColor = UIColor.lightGrayColor
+    label.text = "?"
+    label.font = UIFont.boldSystemFontOfSize(20)
+    label.textColor = UIColor.darkGrayColor
+    label.textAlignment = UITextAlignmentCenter
+    label
+  end
+
+
 end
